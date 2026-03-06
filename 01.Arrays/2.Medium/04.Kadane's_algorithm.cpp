c3cd2903d122dff1bd5dde1644b5ad81673419bd
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2026-03-06 20:00:39 +0530

    Add files via upload

---
+++ 01.Arrays/2.Medium/04.Kadane's_algorithm.cpp
@@ -0,0 +1,68 @@
+/*
+QUESTION:-
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+/*
+APPROACH:-
+-> Initialize two variables: maxSum and currentSum. Set both variables to the first element of the array.
+-> Iterate through the array starting from the second element:
+    Update currentSum by adding the current element to it.
+    If currentSum becomes negative, reset it to 0. This step ensures that we consider only the subarrays with positive sums.
+    Update maxSum by taking the maximum value between maxSum and currentSum. This keeps track of the maximum subarray sum encountered so far.
+-> After the iteration, the maxSum variable will hold the largest sum of any subarray.
+-> Return the maxSum as the result.
+*/
+
+// CODE:-
+int maxSubArray(vector<int> &nums)
+{
+    int curr_sum = 0;
+    int ans = INT_MIN;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        curr_sum += nums[i];
+        ans = max(ans, curr_sum);
+        if (curr_sum < 0)
+            curr_sum = 0;
+    }
+    return ans;
+}
+
+
+int main() {
+    // Test case 1
+    cout << "Test 1:" << endl;
+    vector<int> nums1 = {-2,1,-3,4,-1,2,1,-5,4};
+    int result1 = maxSubArray(nums1);
+    cout << "Output: " << result1 << endl;
+    cout << "Expected: 6" << endl;
+    cout << endl;
+
+    // Test case 2
+    cout << "Test 2:" << endl;
+    vector<int> nums2 = {1};
+    int result2 = maxSubArray(nums2);
+    cout << "Output: " << result2 << endl;
+    cout << "Expected: 1" << endl;
+    cout << endl;
+
+    return 0;
+}
+
+// TIME COMPLEXITY = O(N)
