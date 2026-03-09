01c541093cb1438104bec17bf8b3512ba04f10f6
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2026-03-09 19:01:17 +0530

    Add files via upload

---
+++ 02.Binary Search/1D Arrays/04.Search_insert_position.cpp
@@ -0,0 +1,55 @@
+/*
+QUESTION:
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+/*
+APPROACH:
+We can use the lower_bound function from the C++ standard library to find the index where the target should be inserted. The lower_bound function returns an iterator pointing to the first element that is not less than the target. By subtracting the beginning iterator from the lower_bound iterator, we get the index where the target should be inserted.
+
+CODE:
+*/
+
+int searchInsert(vector<int>& nums, int target) {
+    auto ans = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    return ans;
+}
+
+
+int main() {
+    // Test case 1
+    cout << "Test 1:" << endl;
+    vector<int> nums1 = {1,3,5,6};
+    int target1 = 5;
+    int result1 = searchInsert(nums1, target1);
+    cout << "Output: " << result1 << endl;
+    cout << "Expected: 2" << endl;
+    cout << endl;
+
+    // Test case 2
+    cout << "Test 2:" << endl;
+    vector<int> nums2 = {1,3,5,6};
+    int target2 = 2;
+    int result2 = searchInsert(nums2, target2);
+    cout << "Output: " << result2 << endl;
+    cout << "Expected: 1" << endl;
+    cout << endl;
+
+    return 0;
+}
+
+// TIME COMPLEXITY: O(log n) due to the use of lower_bound function
