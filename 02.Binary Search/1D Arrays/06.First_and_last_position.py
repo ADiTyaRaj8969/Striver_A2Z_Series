4f9b66bce1679516522889d205ce81309f252c47
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2026-03-10 23:26:56 +0530

    Add files via upload

---
+++ 02.Binary Search/1D Arrays/06.First_and_last_position.py
@@ -0,0 +1,7 @@
+# if the target is not found, return [-1, -1]
+# Test case 1
+# TIME COMPLEXITY: O(log n)
+
+def searchRange(& nums, int target): first = lower_bound(nums.begin(), nums.end(), target) - nums.begin()
+# SPACE COMPLEXITY = O(1)
+```
