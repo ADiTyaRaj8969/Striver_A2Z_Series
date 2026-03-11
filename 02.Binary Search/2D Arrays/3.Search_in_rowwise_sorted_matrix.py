4525eb7197fff570635eaca8980ab19503198103
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2026-03-11 23:41:13 +0530

    Add files via upload

---
+++ 02.Binary Search/2D Arrays/3.Search_in_rowwise_sorted_matrix.py
@@ -0,0 +1,8 @@
+# Test case 1
+# Test case 2
+
+def searchMatrix(>& matrix, int target): row = 0, col = matrix[0]).__len__() - 1 while (row < matrix).__len__() and col >= 0) if (matrix[row][col] == target) return true else if (matrix[row][col] > target) col-- else row++ return false
+
+# TIME COMPLEXITY = O(N)
+# SPACE COMPLEXITY = O(1)
+```
