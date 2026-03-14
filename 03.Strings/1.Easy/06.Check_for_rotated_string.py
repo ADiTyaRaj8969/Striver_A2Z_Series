c5fe410907a3b8770beb66cff6b9c8a0f5444d3c
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2026-03-14 21:37:59 +0530

    Add files via upload

---
+++ 03.Strings/1.Easy/06.Check_for_rotated_string.py
@@ -0,0 +1,7 @@
+# Test case 1
+
+def rotateString(string s, string goal): if (s).__len__() not = goal).__len__()) return false if ((s + s).find(goal) == string::npos) return false return true
+
+# TIME COMPLEXITY = O(N)
+# SPACE COMPLEXITY = O(1)
+```
