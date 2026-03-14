e4cb45aa1aae7341b66c12279f1f5afd451df914
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2026-03-14 21:32:03 +0530

    Add files via upload

---
+++ 02.Binary Search/In Search Space/14.Kth_element_of_two_sorted_arrays.py
@@ -0,0 +1,11 @@
+# assuming arr1 is the smaller sized array
+# TODO: Extract test cases from the examples above
+# Call your function: kthElement()
+# Print result:
+# cout << "Result: " << result << endl;
+
+def kthElement(int arr1[], int arr2[], int n, int m, int k): 
+
+# TIME COMPLEXITY = O(N)
+# SPACE COMPLEXITY = O(1)
+```
