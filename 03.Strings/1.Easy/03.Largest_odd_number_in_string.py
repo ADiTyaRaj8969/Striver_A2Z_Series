ecc39f32958930b0466ebb7e5806c8efff54aab4
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2026-03-14 21:35:05 +0530

    Add files via upload

---
+++ 03.Strings/1.Easy/03.Largest_odd_number_in_string.py
@@ -0,0 +1,11 @@
+# Check the first odd number from last
+# TODO: Extract test cases from the examples above
+# Call your function: largestOddNumber()
+# Print string result:
+# cout << "Result: " << result << endl;
+
+def largestOddNumber(string num): 
+
+# TIME COMPLEXITY = O(N)
+# SPACE COMPLEXITY = O(1)
+```
