095df7a58f7211e58348644a72554caee448cf44
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2026-03-15 23:13:50 +0530

    Add files via upload

---
+++ 04.Linked List/1. Single Linked List/01.Intro_to_linked_list.py
@@ -0,0 +1,12 @@
+# Node constructLL(& arr) {
+# Node head = new Node(arr[0]);
+# Node curr = head;
+# TODO: Extract test cases from the examples above
+# Call your function: constructLL()
+# Print linked list/tree (implement a helper function)
+
+Node* constructLL(& arr) Node* head = new Node(arr[0]) Node* curr = head for(int i=1 i<arr).__len__() i++) curr->next = new Node(arr[i]) curr = curr->next return head
+
+# TIME COMPLEXITY = O(N)
+# SPACE COMPLEXITY = O(H)
+```
