27c46affec71988730ed51b7fe657fe16a9e7433
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2026-03-15 22:57:14 +0530

    Add files via upload

---
+++ 03.Strings/2.Medium/03.Roman_to_Integer.cpp
@@ -0,0 +1,75 @@
+/*
+Question:
+Given a Roman numeral as a string, convert it to an integer.
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+
+Approach:
+1. Create a map to store the values of each Roman symbol.
+2. Initialize `result` as the value of the first symbol in the input string.
+3. Iterate through each character `c` in the input string `s`, starting from the second character.
+    a. If the value of the current symbol is greater than the value of the previous symbol, subtract twice the value of the previous symbol from `result` and add the value of the current symbol.
+    b. Otherwise, add the value of the current symbol to `result`.
+4. Return `result` as the converted integer.
+
+Time Complexity: O(n), where n is the length of the input string `s`.
+Space Complexity: O(1)
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int romanToInt(string s) {
+    unordered_map<char, int> symbolValues = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    int result = symbolValues[s[0]];
+    for (int i = 1; i < s.size(); i++) {
+        if (symbolValues[s[i]] > symbolValues[s[i - 1]]) {
+            result -= 2 * symbolValues[s[i - 1]];
+            result += symbolValues[s[i]];
+        } else {
+            result += symbolValues[s[i]];
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    // Test case 1
+    cout << "Test 1:" << endl;
+    string s1 = "III";
+    int result1 = romanToInt(s1);
+    cout << "Output: " << result1 << endl;
+    cout << "Expected: 3" << endl;
+    cout << endl;
+
+    // Test case 2
+    cout << "Test 2:" << endl;
+    string s2 = "LVIII";
+    int result2 = romanToInt(s2);
+    cout << "Output: " << result2 << endl;
+    cout << "Expected: 58" << endl;
+    cout << endl;
+
+    return 0;
+}
+
