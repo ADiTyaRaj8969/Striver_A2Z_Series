6dc9d7ed3a073eaea182e06f4d678e7d4c5c3e29
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2026-03-15 23:02:04 +0530

    Add files via upload

---
+++ 03.Strings/2.Medium/04.Implement_atoi.py
@@ -0,0 +1,13 @@
+# Code:
+# indicates the number is positive
+# ans = (ans  10) + digit;
+# handle overflow case
+# ans = sign ? ans : -1  ans;
+# Test case 1
+# Test case 2
+
+def myAtoi(string s): i = 0 while (i < s).__len__() and s[i] == ' ') i++ if (i == s).__len__()) return 0 sign = true 
+
+# TIME COMPLEXITY = O(N)
+# SPACE COMPLEXITY = O(1)
+```
